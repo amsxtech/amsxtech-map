f1feef158fb4339ddf2a9ed640d376c6a87b753c
PauladO <pauladenouden@gmail.com>
2017-04-24 16:14:26 +0200

    Add dummy data for markers

---
+++ src/seed.js
@@ -0,0 +1,44 @@
+const dummyCompanies = [
+  {
+    name: "Codaisseur",
+    address: "Singel 542, 1017 AZ Amsterdam",
+    coordinates: { lat: "52.3666451", long: "4.8923966" },
+    companyType: "Education",
+    industryType: "IT",
+    hiring: true,
+    website: "https://www.codaisseur.com/",
+    active: true,
+  },
+  {
+    name: "CodeGuild",
+    address: "Herengracht 174, 1016 BR Amsterdam",
+    coordinates: { lat: "52.3739959", long: "4.886718" },
+    companyType: "HR",
+    industryType: "IT",
+    hiring: false,
+    website: "https://www.codeguild.nl/",
+    active: true,
+  },
+  {
+    name: "Impraise",
+    address: "Frederiksplein 1, 1017 XK Amsterdam",
+    coordinates: { lat: "52.3612379", long: "4.8999203" },
+    companyType: "HR",
+    industryType: "IT",
+    hiring: true,
+    website: "https://www.impraise.com/",
+    active: false,
+  },
+  {
+    name: "Guerrilla",
+    address: "Herengracht 601, 1017 CE Amsterdam",
+    coordinates: { lat: "52.3655782", long: "4.8986419" },
+    companyType: "HR",
+    industryType: "IT",
+    hiring: false,
+    website: "https://www.impraise.com/",
+    active: false,
+  },
+]
+
+export default dummyCompanies
