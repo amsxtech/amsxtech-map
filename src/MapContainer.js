5d6f163669e13a48a1d421bb962070cacaf7568b
PauladO <pauladenouden@gmail.com>
2017-04-25 14:32:38 +0200

    rebase

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -4,13 +4,12 @@ import { connect } from 'react-redux'
 import GoogleApiComponent from './GoogleApiComponent'
 import JobsMap from './JobsMap'
 import {Marker} from './Marker'
-import companies from './seed'
+import subscribeToBusinesses from './actions/business/subscribe'
 
 export class MapContainer extends React.Component {
-  componentDidMount() {
-    this.fetchRequests()
-  }
+
   render() {
+    const { companies } = this.props
     return (
       <div>
         <JobsMap google={this.props.google}>
@@ -26,14 +25,14 @@ export class MapContainer extends React.Component {
     )}
 }
 
-// const mapStateToProps = ({ requests }) => {
-//    return (requests.filter((request) => {
-//      request.active
-//     })
-//   )
-// }
+const mapStateToProps = ({ businesses }) => {
+   const companies = businesses.filter((business) => {
+     business.active
+   })
+  return { companies }
+}
 
 let key = config.getGoogleKey()
-export default connect()(GoogleApiComponent({
+export default connect(mapStateToProps, { subscribeToBusinesses })(GoogleApiComponent({
   apiKey: key
 })(MapContainer))
