0082f2b279d9c327140b68f89f9fdf993b78f899
leefreemanxyz <lee.freeman1@gmail.com>
2017-04-26 11:43:28 +0200

    sector and company types can now be added and will populate the drop downs on the map page.

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -3,11 +3,13 @@ import * as config from './config'
 import GoogleApiComponent from './GoogleApiComponent'
 import JobsMap from './JobsMap'
 import {Marker} from './Marker'
+import NavBar from './components/NavBar'
 
 export class MapContainer extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <JobsMap google={this.props.google}>
           <Marker
             title={'Dolores park'}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import currentUser from './currentUser'
 import requests from './requests'
 import businesses from './business'
+import companyTypes from './companyTypes'
+import sectorTypes from './sectorTypes'
 
 module.exports = {
   currentUser,
   requests,
   businesses,
+  companyTypes,
+  sectorTypes,
 }
