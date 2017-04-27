eb287e2a65c32344f8cc842ced6a9a12b8bbe433
anikaSchwing <anikaschwing@gmail.com>
2017-04-27 13:18:23 +0200

    add signout button

--- src/components/NavBarAdmin.js
+++ src/components/NavBarAdmin.js
@@ -1,20 +1,40 @@
-import React, {PureComponent} from 'react'
+import React, {PureComponent, PropTypes} from 'react'
 import { connect } from 'react-redux'
+import signOut from '../actions/user/sign-out'
 
 import AppBar from 'material-ui/AppBar'
 import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
 
 class NavBarAdmin extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool.isRequired,
+  }
+
+  signOut(event) {
+    event.preventDefault()
+    this.props.signOut()
+  }
+
   render(){
-    const {companyTypes, sectorTypes } = this.props
+    const { signedIn } = this.props
 
     return (
       <div>
         <AppBar title="AMSxTech Map - Admin" showMenuIconButton={false}>
+          { signedIn ?
+            <li><br /><RaisedButton label="Sign Out" onClick={this.signOut.bind(this)} /></li> :
+            null
+          }
 
         </AppBar>
       </div>
     )
   }
 }
-export default NavBarAdmin
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: (!!currentUser && !!currentUser._id)
+})
+
+export default connect(mapStateToProps, { signOut })(NavBarAdmin)
