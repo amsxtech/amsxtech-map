90e2c985856a59f51517d00eb1ca0ccc0c0eb471
anikaSchwing <anikaschwing@gmail.com>
2017-04-28 12:03:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { ROOT_PATH, USER_SIGN_UP_PATH, USER_SIGN_IN_PATH, ADD_BUSINESS_PATH, BUS
 import RequestBusiness from './components/requestBusiness'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+import RequestsIndex from './components/requestsIndex'
 
 ReactDOM.render(
   <Provider store={store}>
