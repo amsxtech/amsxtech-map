b5e23566d31b5ba4d986779ef0eb5fcaace774bb
anikaSchwing <anikaschwing@gmail.com>
2017-04-28 11:58:16 +0200

    changed fontsize, removed unused route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { ROOT_PATH, USER_SIGN_UP_PATH, USER_SIGN_IN_PATH, ADD_BUSINESS_PATH, BUS
 import RequestBusiness from './components/requestBusiness'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
-import RequestsIndex from './components/requestsIndex'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,7 +19,6 @@ ReactDOM.render(
         <Route path={BUSINESSES_INDEX_PATH} component={RequestsIndex} />
         <Route path={USER_SIGN_IN_PATH} component={SignIn} />
         <Route path={USER_SIGN_UP_PATH} component={SignUp} />
-        <Route path={ADD_BUSINESS_PATH} component={RequestBusiness} />
       </Route>
     </Router>
   </Provider>,
